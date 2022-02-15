174ee94aca6b11793802ca88fad6d41d6d0ca893
智哉 渡部 <190078@jc-21.jp>
2022-02-15 15:23:11 +0900

    プロジェクト ファイルを追加します。

---
+++ JetCoaster.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class JetCoaster
+    {
+        public static int ride(int k,int[] g,int r)
+        {
+            int b = 0 ;//添え字
+            int all = 0; //搭乗人数
+            int cnt = 0;　//現在の添字
+            int c = 0;
+            for(int z = 0; z < r; z++)
+            {                      
+
+
+                for(int y = cnt; y <= g.Length; y++)
+                {
+                    if (y > g.Length)
+                    {
+                        y--;
+                    }
+                    if (b  > k) //定員
+                    {
+                        b = c;
+                        c = 0;
+                        all = all + b;
+                        b=c;
+                        cnt = y-1; 
+                        break;
+                    }
+                    c = b;
+                    b = b + g[y];
+
+                    if (y + 1 == g.Length && b > k)
+                    {
+                        z--;
+                        c = b;
+                        break;
+                    }else if (y + 1 == g.Length)
+                    {
+                        y = -1;
+                        cnt = 0;
+                    }
+                    else
+                    {
+
+                    }
+
+                }                
+
+            }           
+
+            return all;
+        }
+    }
+}
---
+++ kuriage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class kuriage
+    {
+        public static int kuriage10(int i)
+        {
+            int x, y;
+
+            x = 0;
+            y = 0;
+
+            if (i < 0)
+            {
+                i = i * -1;
+                x = i % 10;
+                y = i - x;
+                i = y + 10;
+                i = i * -1;
+                return i;
+            }
+            x = i % 10;
+            if(x==0){
+                return i;
+            }
+            else
+            {
+                y = i - x;
+                i = y + 10;
+                return i;
+            }
+        }
+    }
+}
